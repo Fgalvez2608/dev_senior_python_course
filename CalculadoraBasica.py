3968886c1637be30b72327997cfffab2d374281a
FGALVEZ2608 <freddygalvezgallego@gmail.com>
2024-10-24 23:21:40 -0500

    Calculadora Basica
    Calculadora basica con comentarios de codigo y de 2 formas de uso

--- CalculadoraBasica.py
+++ CalculadoraBasica.py
@@ -1,5 +1,5 @@
 print("Bienvenido a la Calculadora Basica")
-
+#funcion que coonvierte el parametro de entrada en flotante y retorna: True y si no se puede convertir retorna:
 def isFloat(value):
     try:
         float(value)
